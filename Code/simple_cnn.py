bff82ae8724293b4a56ec2934083c9785f96f200
Pratik Bhujbal <bhujbalpratik97@gmail.com>
2022-12-02 02:08:50 -0500

    added class for simple cnn method

---
+++ Code/simple_cnn.py
@@ -0,0 +1,63 @@
+import numpy as np
+import seaborn as sn
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+
+class Simple_CNN(tf.keras.Model):
+    def __init__(self, num_classes):
+        super().__init__()
+        self.conv1 = tf.keras.layers.Conv2D(
+            32,
+            kernel_size=5,
+            strides=1,
+            activation='relu',
+            padding='same')
+        self.conv2 = tf.keras.layers.Conv2D(
+            32,
+            kernel_size=3,
+            strides=1,
+            activation='relu',
+            padding='same')
+        self.m1 = tf.keras.layers.MaxPool2D()
+        self.conv3 = tf.keras.layers.Conv2D(
+            64,
+            kernel_size=3,
+            activation='relu',
+            padding='same')
+        self.m2 = tf.keras.layers.MaxPool2D()
+        self.flatten = tf.keras.layers.Flatten()
+        self.dense1 = tf.keras.layers.Dense(512, activation='relu')
+        self.dense2 = tf.keras.layers.Dense(256, activation='relu')
+        self.dense3 = tf.keras.layers.Dense(128, activation='relu')
+        self.dense4 = tf.keras.layers.Dense(64, activation='relu')
+        self.dense5 = tf.keras.layers.Dense(num_classes, activation='softmax')
+
+    def call(self, input):
+        out = self.conv1(input)
+        out = self.conv2(out)
+        out = self.m1(out)
+        out = self.conv3(out)
+        out = self.m2(out)
+        out = self.flatten(out)
+        out = self.dense1(out)
+        out = self.dense2(out)
+        out = self.dense3(out)
+        out = self.dense4(out)
+        out = self.dense5(out)
+        return out
+
+    def optimizer(self, model):
+        """Specifying a loss function, an optimizer, and metrics to monitor.
+
+        Args:
+            model : model
+
+        Returns:
+            _type_: Compiled model
+        """
+        return model.compile(
+            optimizer='adam',
+            loss="categorical_crossentropy",
+            metrics=['accuracy'])
+
